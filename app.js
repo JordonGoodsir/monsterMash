2c4cc4a55c309eefa1ee010137534cad85d79b6c
JordonGoodsir <jordon.goodsir1@gmail.com>
2020-10-21 10:01:23 +1100

    port env added

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ require("./middleware/passport")
 
 
 // set up 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();      
 
 require('dotenv').config()
